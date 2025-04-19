c7ee3353b012331868f5accf77c8ddcd987c068b
Lynchlee <noslynchlee@gmail.com>
2025-04-19 15:16:40 +0700

    ok

--- src/interface/news.tsx
+++ src/interface/news.tsx
@@ -1,11 +1,11 @@
-            export interface Inews {
-                _id: string;
-                title: string;
-                img: string[];
-                content: string;
-                descriptions: string;
-            }
-            export type InewsLite = Pick<
-            Inews,
-                "_id" | "title" | "img" | "content" | "descriptions" 
-            >;
+export interface Inews {
+  _id: string;
+  title: string;
+  img: string[];
+  content: string;
+  descriptions: string;
+}
+export type InewsLite = Pick<
+  Inews,
+  "_id" | "title" | "img" | "content" | "descriptions"
+>;
