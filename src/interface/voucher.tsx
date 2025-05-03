da24a679db6bdb4ae5cbe3b63f3f629ee477c39f
Dangbac2004 <bacdxph36842@fpt.edu.vn>
2025-05-03 22:39:38 +0700

    test lai ative

--- src/interface/voucher.tsx
+++ src/interface/voucher.tsx
@@ -1,9 +1,8 @@
 export interface IVoucher {
-    _id?: string; // Optional for when the voucher is created
-    code: string;
-    discountAmount: number;
-    expirationDate: Date;
-    isActive: boolean;
-    quantity: number;
-    createdAt?: Date; // Optional as it will be auto-generated
-  }
+  _id?: string; // Optional for when the voucher is created
+  code: string;
+  discountAmount: number;
+  expirationDate: Date;
+  quantity: number;
+  createdAt?: Date; // Optional as it will be auto-generated
+}
