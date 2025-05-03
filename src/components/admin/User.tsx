3ba5083f599002fd3c9bbf80412a3a2d64439846
dotu <dotu2310@gmail.com>
2025-05-03 19:01:59 +0700

    vo hieu hoa lai tk

--- src/components/admin/User.tsx
+++ src/components/admin/User.tsx
@@ -57,19 +57,6 @@ const Users = (props: Props) => {
     }
   };
 
-  useEffect(() => {
-    const fetchDeactivationHistory = async () => {
-      try {
-        const data = await getDeactivationHistory();
-        setDeactivationHistory(data);
-      } catch (error) {
-        console.error("Error fetching deactivation history:", error);
-      }
-    };
-    fetchUsers();
-    fetchDeactivationHistory();
-  }, []);
-
   const deactivateUserById = async (id: string) => {
     let selectedReasonLocal = "";
     let tempReason = "";
